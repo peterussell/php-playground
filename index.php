0243f63aab7b3f6c4c6c9fdb57e6ac25f78d5bcf
Pete Russell <pinkbatz@gmail.com>
2020-01-19 21:50:47 +1300

    Adds querystring get demo.

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<!DOCTYPE html>
 
-echo 'Hello World';
+<html lang="en">
 
- ?>
+<head>
+  <meta charset="utf-8">
+  <title>Get some!!!</title>
+  <style>
+    header {
+      background: #e3e3e3;
+      padding: 2em;
+      text-align: center;
+    }
+  </style>
+</head>
+
+<body>
+  <header>
+    <h1>
+      <?php
+        $name = $_GET['name'];
+        echo "hey {$name} -- I want you to GET SOME";
+      ?>
+    </h1>
+  </header>
+</body>
